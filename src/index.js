9b2afec31c2fa468eecb0a3a427a78f19aa17ef4
Anton Zakhar <e725f1d26ac8041f@gmail.com>
2021-02-22 19:45:45 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openingBrackets = bracketsConfig.map(bracketsPair => bracketsPair[0]);
+  const closingBrackets = bracketsConfig.map(bracketsPair => bracketsPair[1]);
+  
+  const bracketsSequence = str.split('');
+  const openingBracketsStack = new Array();
+  for (let currentBracket of bracketsSequence) {
+    let lastOpeningBracket = openingBracketsStack[openingBracketsStack.length - 1];
+    if (openingBrackets.indexOf(currentBracket) === closingBrackets.indexOf(currentBracket)) {
+      if (currentBracket === lastOpeningBracket) {
+        openingBracketsStack.pop();
+      } else {
+        openingBracketsStack.push(currentBracket);
+      }
+      continue;
+    }
+
+    if (openingBrackets.includes(currentBracket)) {
+      openingBracketsStack.push(currentBracket);
+    } else {
+      const currentClosingBracket = currentBracket;
+      if (closingBrackets.indexOf(currentClosingBracket) === openingBrackets.indexOf(lastOpeningBracket)) {
+        openingBracketsStack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return openingBracketsStack.length === 0;
 }
